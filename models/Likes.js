21dd3263262bbd412e4d8b55d52ad4b0e8f642b7
Jose Padilla <quito1970@gmail.com>
2024-07-10 10:09:16 -0400

    changesServer

---
+++ models/Likes.js
@@ -0,0 +1,9 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    const Likes = sequelize.define("Likes",{
+        
+       
+    });
+    return Likes;
+}
